53eb286501895086f53cea14adee57ee5f6a3e15
jenifer-brown <ID+USERNAME@users.noreply.github.com>
2022-12-16 16:02:37 -0500

    vite config

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jenifer-brown/jenifer-brown.github.io.git main
+# git push -f git@github.com:jenifer-brown/jenifer-brown.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:jenifer-brown/WebApp.git main:gh-pages
 
 cd -
