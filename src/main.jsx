164d9db75c9be6ba09978bff9d3ef1149e64fc0f
jenifer-brown <ID+USERNAME@users.noreply.github.com>
2022-12-16 17:53:04 -0500

    Updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-import App from './App'
-import './index.css'
-
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#maincontent"));
 root.render(<App />);
-
